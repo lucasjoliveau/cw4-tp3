b2d9377d91509b30c48ad8ae25b23d0812821cde
Lucas Joliveau <lucas@joliveau.com>
2020-05-21 23:18:33 -0400

    Ajout des animations

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,19 @@
 document.addEventListener("DOMContentLoaded", function(event) {
+
+    // Intialisation de WOW.js pour animer les textes
+    wow = new WOW(
+        {
+            boxClass:     'wow',      // default
+            animateClass: 'animated', // change this if you are not using animate.css
+            offset:       0,          // default
+            mobile:       true,       // keep it on mobile
+            live:         true        // track if element updates
+        }
+    )
+    wow.init();
+
+
+
     // Menu de navigation Hamburger
     const hamburger = document.querySelector(".hamburger");
     const navLinks = document.querySelector(".nav-links");
